f50693db99c83592c40eb61d395c038dd585212f
NormalPeeple <fikrifauzy156@gmail.com>
2023-05-31 12:12:43 +0700

    first commit

---
+++ Mobile Development/bangkit-2023-capstone-MD/app/src/main/java/com/example/capstone/RegistActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.capstone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regist2)
+    }
+}
---
+++ Mobile Development/readme.txt
@@ -0,0 +1,2 @@
+Figma
+https://www.figma.com/file/vuqdLvke6bfHHX7vPQDBA9/Capstone-Bangkit-2023?type=design&node-id=0%3A1&t=BJSDoUVBctpeSQgX-1
